459d0adcbd4b1d82287096f7468c732c03f999e9
Vice <vicerust@gmail.com>
2022-01-17 01:21:24 -0500

    Init

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VirtualDesktop;
+
+namespace BetterVirtualDesktops
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args) 
+        {
+            HotKeyManager.RegisterHotKey(Keys.Up, KeyModifiers.Control | KeyModifiers.Windows);
+            //HotKeyManager.RegisterHotKey(Keys.Down, KeyModifiers.Control | KeyModifiers.Windows);
+
+            HotKeyManager.HotKeyPressed += (obj, args) =>
+            { 
+                switch (args.Key) 
+                {
+                    case Keys.Up: 
+                    {
+                        SwapDesktop();
+                        break;
+                    }
+                    case Keys.Down:
+                    {
+                        
+                        break;
+                    }
+                }
+            };
+
+            Thread.Sleep(-1);
+        }
+
+
+        public static void SwapDesktop()
+        {
+            try
+            {
+                //DesktopHelper.SetLockForWindowsOnMonitor(@"\\.\DISPLAY3");
+                var desktop = GetNextDesktop();
+                
+                if ((Control.MouseButtons & MouseButtons.Left) != 0)
+                    desktop.MoveActiveWindow();
+                
+                desktop.MakeVisible();
+            }
+            catch (Exception e)
+            { 
+                Console.WriteLine(e);
+            }
+        }
+        
+        public static Desktop CurrentDesktop => Desktop.FromIndex(Desktop.FromDesktop(Desktop.Current));
+        
+        public static Desktop GetNextDesktop()
+        {
+            int desktopCount = Desktop.Count;
+            int visibleDesktop = Desktop.FromDesktop(VirtualDesktop.Desktop.Current);
+
+            // round robin desktops
+            int target = (visibleDesktop + 1) % desktopCount; 
+            
+            return VirtualDesktop.Desktop.FromIndex(target);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-BetterVirtualDesktops
+# Better Virtual Desktops
+
+Pretty simple. 
+
+- Registers one hotkey, `Control + Win + UpArrow`, which will round-robin cycle through desktops. Can be bound to CapsLock hyperkeys or mouse buttons.
+- If your mouse is down (i.e., you're dragging a window) the window will be seamlessly transported to the new desktop, letting you drag windows between desktops without the task switcher.
+
+Uses C# native bindings for the [Windows 11 Virtual Desktop API from @MScholtes](https://github.com/MScholtes/VirtualDesktop). This is an undocumented API, so it can break at any point in the future.
+
+You should be able to swap out the `VirtualDesktop.cs` for the Windows 10 version, but I'm on Windows 11 so I cannot test it.
